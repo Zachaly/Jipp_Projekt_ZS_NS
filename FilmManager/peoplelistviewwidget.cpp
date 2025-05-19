3d77a52dadd667905f53f4817512993b9f20b183
natisajdak <nati.sajdak@wp.pl>
2025-05-19 15:21:14 +0200

    Update peoplelistviewwidget.cpp

--- FilmManager/peoplelistviewwidget.cpp
+++ FilmManager/peoplelistviewwidget.cpp
@@ -3,6 +3,10 @@
 #include "personviewwidget.h"
 #include "startviewwidget.h"
 #include "ui_peoplelistviewwidget.h"
+#include "FilmManager_Domain/personmanager.h"
+
+#include <QMessageBox>
+#include <QListWidgetItem>
 
 
 PeopleListViewWidget::PeopleListViewWidget(MainWindow *parent)
@@ -10,9 +14,12 @@ PeopleListViewWidget::PeopleListViewWidget(MainWindow *parent)
     , ui(new Ui::PeopleListViewWidget)
 {
     ui->setupUi(this);
-    connect(ui->personButton, SIGNAL(clicked()), this, SLOT(goToPerson()));
-    connect(ui->addPersonButton, SIGNAL(clicked()), this, SLOT(addPerson()));
-    connect(ui->goBackButton, SIGNAL(clicked()), this, SLOT(goBack()));
+
+    connect(ui->goBackButton,    &QPushButton::clicked,            this, &PeopleListViewWidget::goBack);
+    connect(ui->addPersonButton, &QPushButton::clicked,            this, &PeopleListViewWidget::addPerson);
+    connect(ui->peopleListWidget,&QListWidget::itemDoubleClicked,  this, &PeopleListViewWidget::goToPerson);
+
+    refreshPeopleList();
 }
 
 PeopleListViewWidget::~PeopleListViewWidget()
@@ -20,21 +27,50 @@ PeopleListViewWidget::~PeopleListViewWidget()
     delete ui;
 }
 
+void PeopleListViewWidget::refreshPeopleList()
+{
+    ui->peopleListWidget->clear();
+
+    for (const auto& p : PersonManager::getPeople()) {
+        QString label = toQString(p.getFirstName() + " " + p.getLastName());
+        auto* item = new QListWidgetItem(label);
+        item->setData(Qt::UserRole, toQString(p.getId()));
+        ui->peopleListWidget->addItem(item);
+    }
+}
+
 void PeopleListViewWidget::goToPerson()
 {
-    auto person = Person();
-    ((MainWindow*)parent())->changePage(new PersonViewWidget(person, (MainWindow*)parent()));
+    auto* item = ui->peopleListWidget->currentItem();
+    if (!item) {
+        QMessageBox::warning(this, "No Selection", "Please select a person from the list.");
+        return;
+    }
+
+    std::string id = fromQString(item->data(Qt::UserRole).toString());
+    Person& p = PersonManager::getPersonById(id);
+
+
+    auto* editView = new PersonViewWidget(p, qobject_cast<MainWindow*>(parent()));
+    qobject_cast<MainWindow*>(parent())->changePage(editView);
 }
 
 void PeopleListViewWidget::addPerson()
 {
-    auto dialog = new AddPersonDialog(this);
-    dialog->show();
-    dialog->raise();
-    dialog->activateWindow();
+    auto* dlg = new AddPersonDialog(this);
+    connect(dlg, &AddPersonDialog::personAdded,
+            this, &PeopleListViewWidget::refreshPeopleList);
+
+    if (dlg->exec() == QDialog::Accepted) {
+        QMessageBox::information(this, "Success", "Person added successfully.");
+    }
+    delete dlg;
 }
 
 void PeopleListViewWidget::goBack()
 {
-    ((MainWindow*)parent())->changePage(new StartViewWidget((MainWindow*)parent()));
+    auto* mw = qobject_cast<MainWindow*>(parent());
+    if (mw) {
+        mw->changePage(new StartViewWidget(mw));
+    }
 }
