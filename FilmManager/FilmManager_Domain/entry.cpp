68b14d30c68726c37f1a2dbd4a7f0b6c1fc294c8
Zachary Soja <zachary3003@gmail.com>
2025-05-23 16:13:07 +0200

    add id generation to entry

--- FilmManager/FilmManager_Domain/entry.cpp
+++ FilmManager/FilmManager_Domain/entry.cpp
@@ -1,7 +1,9 @@
 #include "entry.h"
+#include <ctime>
+#include <string>
 
 Entry::Entry() {
-    this->id = "random"; //TODO: generate random id
+    this->id = generateId();
 }
 
 Entry::Entry(string id)
@@ -11,4 +13,21 @@ Entry::Entry(string id)
 
 string Entry::getId() {
     return id;
+}
+
+string Entry::generateId()
+{
+    srand((unsigned)time(NULL));
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    std::string tmp_s;
+    tmp_s.reserve(50);
+
+    for (int i = 0; i < 50; ++i) {
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+
+    return tmp_s;
 }
