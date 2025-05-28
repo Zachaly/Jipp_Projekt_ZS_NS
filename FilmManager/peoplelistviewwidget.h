20190424b4e5a7f259cd4f19427baa9e417880ac
natisajdak <nati.sajdak@wp.pl>
2025-05-28 10:43:19 +0200

    Update peoplelistviewwidget.h

--- FilmManager/peoplelistviewwidget.h
+++ FilmManager/peoplelistviewwidget.h
@@ -2,7 +2,11 @@
 #define PEOPLELISTVIEWWIDGET_H
 
 #include "mainwindow.h"
+#include "FilmManager_Domain/person.h"
 #include <QWidget>
+#include <vector>
+
+using namespace std;
 
 namespace Ui {
 class PeopleListViewWidget;
@@ -21,9 +25,20 @@ private slots:
     void addPerson();
     void goBack();
     void refreshPeopleList();
+    void onFilterChanged();
 
 private:
+    void setupConnections();
+    void setupListWidget();
+    void displayPeople(const vector<Person>& people);
+    void createPersonListItem(const Person& person);
+    void updatePeopleList();
+    vector<Person> getFilteredPeople();
+    QString getRoleIcon(const Person& person);
+    QString getRoleText(const Person& person);
+
     Ui::PeopleListViewWidget *ui;
+    vector<Person> currentPeople;
 };
 
 #endif // PEOPLELISTVIEWWIDGET_H
