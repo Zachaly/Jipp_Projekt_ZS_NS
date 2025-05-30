671bce1e0bd65297aee947bf035f3e5cffb2c503
Zachary Soja <zachary3003@gmail.com>
2025-05-30 02:34:33 +0200

    add saving people

--- FilmManager/main.cpp
+++ FilmManager/main.cpp
@@ -1,10 +1,11 @@
 #include "mainwindow.h"
-
+#include "FilmManager_Domain/personmanager.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    PersonManager::loadFromFile();
     app.setApplicationName("Film & Series Manager");
     app.setApplicationVersion("1.0");
     MainWindow w;
